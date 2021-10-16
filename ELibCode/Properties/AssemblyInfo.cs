220864d0e4f2e35405ec372158f6f8935190c65c
limited_dev <74376181+limited-dev@users.noreply.github.com>
2021-10-16 21:40:16 +0200

    Changes in Proerties

--- ELibCode/Properties/AssemblyInfo.cs
+++ ELibCode/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ELibCode")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ELib")]
+[assembly: AssemblyDescription("A Lib made for use @ school.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ELibCode")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("limited_dev")]
+[assembly: AssemblyProduct("ELib")]
+[assembly: AssemblyCopyright("Copyright limited_dev ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
